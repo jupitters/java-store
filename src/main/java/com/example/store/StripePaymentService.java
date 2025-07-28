e2ba8378ceea5170412af30eb89fafed627f694b
jupitters <sampaiobale@gmail.com>
2025-07-28 16:48:13 -0300

    services anottations

--- src/main/java/com/example/store/StripePaymentService.java
+++ src/main/java/com/example/store/StripePaymentService.java
@@ -1,5 +1,7 @@
 package com.example.store;
 
+import org.springframework.stereotype.Service;
+
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
