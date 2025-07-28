26fef1b78cd0db6ec3a95fddf2902418a739c89b
jupitters <sampaiobale@gmail.com>
2025-07-28 16:11:01 -0300

    interface

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -2,6 +2,7 @@ package com.example.store;
 
 public class OrderService {
     public void placeOrder() {
-
+        var paymentService = new StripePaymentService();
+        paymentService.processPayment(10);
     }
 }
